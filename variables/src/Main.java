d073e9270222b79f044f6d44a5364163a328f3a8
talhaaauysalll <talha.uysal@outlook.com.tr>
2022-09-23 10:34:41 +0300

    Kodda düzenlemeler yapıldı.

--- variables/src/Main.java
+++ variables/src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args){
         int ogrenciSayisi=5;
-        System.out.println("Öğrenci sayısı= "+ogrenciSayisi);
+        String mesaj="Öğrenci Sayısı : ";
+        System.out.println(mesaj+ogrenciSayisi);
     }
 }
