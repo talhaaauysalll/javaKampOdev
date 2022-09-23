cc1f1942a8523917ae5fee9d495c6a5bb7711ebf
talhaaauysalll <talha.uysal@outlook.com.tr>
2022-09-23 10:59:28 +0300

    Değişken tipleri hakkında bilgiler elde edildi.

---
+++ datatypes/datatypes/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args){
+        //primitive types
+        //boolean, char, byte, int, double, float, int ve double daha çok kullanılır
+        //TC kimlik metinsel tutulursa mantıklıdır. (Best Practice)
+        //char tek karakter ve 2 byte'lık bir veri tutar.
+        //boolean 1 byte'lık bir veri tutar. true ve false
+        //char veri tipide bir sayıdır. Arkaplanda sayısal karşılıkları vardır.
+
+    }
+}
